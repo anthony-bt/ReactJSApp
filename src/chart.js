2eb727725d9dd29af5152d96128b74fc64e62379
Anthony Barillot <anthony.barillot@gmail.com>
2025-03-02 20:39:58 -0300

    Remove unused code in Chart component

--- src/chart.js
+++ src/chart.js
@@ -11,33 +11,26 @@ const data = [
   { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
 ];
 
-export default function Chart(
-  style = {
-    windth: 300,
-    height: 200
-  }
-) {
+export default function Chart() {
   return (
-    // <ResponsiveContainer width="100%" height="100%">
-      <LineChart
-        width={300}
-        height={200}
-        data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-      </LineChart>
-    // </ResponsiveContainer>
+    <LineChart
+      width={300}
+      height={200}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+    </LineChart>
   )
 }
